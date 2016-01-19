fb49d60555a45e3ef848f9a1de84def46b67acde
Yannis Mentekidis <mentekid@gmail.com>
2016-01-19 16:19:36 +0200

    Adds dpcm decorrelation functionality

--- adpcm.m
+++ adpcm.m
@@ -1,1 +1,29 @@
-%I changed this file beyond recognition and you can't do anything about it
+function [rq, wq] = adpcm(x, D, L, m, wmin, wmax, n)
+x = x';
+%% Prepare coding system
+%Compute m-degree w for x
+w = lpcoeffs(x, m);
+
+%Compute quantization parameters for W
+[wD, wL] = quantLevels(n, wmin, wmax);
+
+
+%% Mimic decoder quantization noise
+%for signal
+xr = arrayfun(@(sample) iQuant(Quant(sample, D), L), x);
+
+%for predictor coefficients
+wq = arrayfun(@(sample) iQuant(Quant(sample, wD), wL), w);
+
+%% Decorrelate signal by predicting its values
+rq = zeros(size(x));
+rq(1:m) = xr(1:m);
+
+for i=m+1:length(x),
+    prediction = wq'*xr(i-1:-1:i-m);
+    rq(i) = xr(i) - prediction;
+end
+
+%% Quantize residuals and w
+rq = arrayfun(@(sample) Quant(sample, D), rq);
+wq = arrayfun(@(sample) Quant(sample, wD), wq);
---
+++ iadpcm.m
@@ -0,0 +1,18 @@
+function xd = iadpcm(rq,wq,L,wmin,wmax,n)
+m = length(wq);
+N = length(rq);
+
+%% Recover residuals and prediction coefficients
+[~, wL] = quantLevels(n, wmin, wmax);
+w = arrayfun(@(sample) iQuant(sample, wL), wq);
+r = arrayfun(@(sample) iQuant(sample, L), rq);
+
+%% Reverse the dpcm process
+xd = zeros(size(r));
+xd(1:m) = r(1:m); %first samples are not decorrelated
+
+for i=m+1:N,
+    pred = w' * xd(i-1:-1:i-m);
+    xd(i) = pred + r(i);
+end
+xd = xd';
